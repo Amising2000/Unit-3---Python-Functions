d58ab70941c5b0d5738e5e48dbb9fdba6f93cced
Angad <angadveerasija@gmail.com>
2025-11-23 14:50:23 -0500

    lesson5 exceptions

---
+++ Lesson5 - Exception Handling/common_exceptions.py
@@ -0,0 +1,36 @@
+# Common Errors
+# 1 - Division by zero
+# print(10/0)
+
+# 2 - ValueError
+# print(int("abc"))
+
+# 3 - KeyError
+# grades = {"Bob": 90}
+# print(grades["Bill"])
+
+# 4 - IndexError
+# numbers = [1, 2, 3]
+# print(numbers[5])
+
+# 5 - NameError
+# print(total)
+# total = 10
+
+# 6 - SyntaxError
+# for i in range(5)
+# print(i)
+
+# 7 - TypeError
+# print("hello" + 5)
+
+# 8 - AttrbuteError
+# x = 5
+# x.append(3)
+
+# 9 - ModuleNotFoundError
+# import wonka_Factory
+
+# 10 - IndentationError
+# if i > 0:
+# return 1
--- Lesson5 - Exception Handling/exceptions.py
+++ Lesson5 - Exception Handling/exceptions.py
@@ -1,22 +1,61 @@
-# Common Errors
-# 1 - Division by zero
-# print(10/0)
+def safe_divide(x, y):
+    try:
+        result = x / y
+        return result
+    # except:
+    #     print("Cannot Divide by Zero!")
+    #     return None
+    except ZeroDivisionError:
+        print("Cannot Divide by Zero!")
+        return None
+    except TypeError:
+        print("That's Not A Valid Number!")
+        return None
+    except:
+        print("An error has occured...")
+        return None
 
-# 2 - ValueError
-# print(int("abc"))
+print(safe_divide(10, 2))
+print(safe_divide(10, 0))
+print(safe_divide(10, "hello"))
 
-# 3 - KeyError
-# grades = {"Bob": 90}
-# print(grades["Bill"])
 
-# 4 - IndexError
-# numbers = [1, 2, 3]
-# print(numbers[5])
+def safe_operations(a, b, lst, key, d):
+    try:
+        print(f"Divition results: {a/b}")
+        TypeError
+        print("Access List Element:", lst[2])
+        print("Access Dictionary Key:", d[key])
+        print("Add Numbers: {a + b}")
+    except ZeroDivisionError:
+        print("Cannot divide by zero!")
+    except IndexError:
+        print("List index out of range!")
+    except KeyError:
+        print(f"Key {key} not found in dictionary!")
+    except TypeError:
+        print("Invalid types for operation!")
+    except Exception as e:
+        print("Some other error occured", e)
 
-# 5 - NameError
-# print(total)
-# total = 10
+print(safe_operations(10, 2, [1, 2], "Tom", {"John": 15}))
 
-# 6 - SyntaxError
-# for i in range(5)
-# print(i)
+# 1
+def calculate_price_per_item(total_cost, num_items):
+    try:
+        result = total_cost / num_items
+    except Exception as e:
+        result =  print("Some error occured:", e)
+    return result
+
+print(calculate_price_per_item(100, 4))
+print(calculate_price_per_item(50, 0))
+print(calculate_price_per_item(25.50, 3))
+
+# 2
+def parse_age(string):
+    try:
+        pasre = int(string)
+        return
+    except:
+        return None
